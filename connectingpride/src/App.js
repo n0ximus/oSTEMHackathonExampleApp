f75ea312d51b8e7840002c083791131ae3e0117d
n0ximus <SageM5862@gmail.com>
2022-09-30 22:24:04 -0700

    navbar, header, homepage

--- connectingpride/src/App.js
+++ connectingpride/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import NavBar from './components/navbar/navbar'
+import Home from './pages/home/home'
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar/>
+      <Home/>
+
     </div>
   );
 }
---
+++ connectingpride/src/pages/home/home.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./home.css"
+import Header from "../../components/header/header"
+export default function Home() {
+
+    return (
+        <>
+          <Header />
+          <div className="home">
+
+          </div>
+        </>
+    ) 
+}
