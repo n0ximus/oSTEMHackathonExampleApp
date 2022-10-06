6627afbe4ae6a1b1ff313663debdfa64b59aed1c
n0ximus <SageM5862@gmail.com>
2022-10-05 23:12:09 -0700

    post thumbnails

--- connectingpride/src/pages/home/home.jsx
+++ connectingpride/src/pages/home/home.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import "./home.css"
 import Header from "../../components/header/header"
+import Posts from "../../components/posts/posts"
 export default function Home() {
 
     return (
         <>
           <Header />
           <div className="home">
-
+            <SideBar />
+            <Posts/>
           </div>
         </>
     ) 
